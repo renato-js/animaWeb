724c0bbbbbc874adbc314f7e948bbffddff5b937
renatojs <renato@grupoartway.com.br>
2013-09-09 13:59:58 -0300

    version with content inside body

--- src/js/animaWeb.js
+++ src/js/animaWeb.js
@@ -9,7 +9,7 @@
 */
 
 // HOW TO USE
-// animaWeb(elemento, posicao-inicial-X, posicao-final-X, animacao)
+// animaWeb(elemento, posicao-inicial-X, posicao-final-X, animacao, x inicial, x final, y inicial, y final)
 // size
 // position
 
@@ -20,17 +20,19 @@ var posX;
 
 function onScroller() {
 	posX = document.body.scrollTop;
+	console.log(posX);
 	animaWeb();
 }
 
 
 //animacao de temporizoacao
-function anima (_elemento, _posIni, _posEnd, _animacao, _tamanhoInicial, _tamanhoFinal) {
+function anima (_elemento, _posIni, _posEnd, _animacao, _xInit, _xEnd, _yInit, _yEnd ) {
 	var _e = document.getElementById(_elemento);
-	var _cada = (_tamanhoFinal-_tamanhoInicial)/(_posEnd-_posIni);			//pega quantos pixels vai aumentar da logo por pixel animado
 
+	//tamanho
 	if(_animacao == "size")
 	{	
+		var _cada = (_xEnd-_xInit)/(_posEnd-_posIni);			//pega quantos pixels vai aumentar da logo por pixel animado
 		if(posX > _posIni && posX < _posEnd)
 		{
 			//faz animacao de transicao
@@ -41,15 +43,46 @@ function anima (_elemento, _posIni, _posEnd, _animacao, _tamanhoInicial, _tamanh
 		{
 			//mostra versao pequena
 			_e.style.backgroundColor = "red";
-			_e.style.height = _tamanhoInicial;
+			_e.style.height = _xInit;
 		}
 		else if(posX > _posEnd)
 		{
 			//mostra versao grande
 			_e.style.backgroundColor = "red";
-			_e.style.height = _tamanhoFinal;			
+			_e.style.height = _xEnd;			
 		}
 	}
+
+
+	if(_animacao == "posicao")
+	{	
+		var _cadaX = (_xEnd-_xInit)/(_posEnd-_posIni);			//pega quantos pixels vai aumentar da logo por pixel animado
+		var _cadaY = (_yEnd-_yInit)/(_posEnd-_posIni);			//pega quantos pixels vai aumentar da logo por pixel animado
+		
+		if(posX > _posIni && posX < _posEnd)
+		{
+			//faz animacao de transicao
+			_e.style.top = ((posX-_posIni)*_cadaX)+"px";
+			_e.style.left = ((posX-_posIni)*_cadaY)+"px";
+			_e.style.backgroundColor = "gray";			
+		}
+		else if(posX < _posIni)
+		{
+			//mostra versao pequena
+			_e.style.backgroundColor = "red";
+			_e.style.top = _xInit;
+			_e.style.left = _yInit;
+		}
+		else if(posX > _posEnd)
+		{
+			//mostra versao grande
+			_e.style.backgroundColor = "red";
+			_e.style.top = _xEnd;			
+			_e.style.left = _yEnd;			
+		}
+	}
+
+
 }
 
 
