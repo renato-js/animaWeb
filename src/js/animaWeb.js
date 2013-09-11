76b14a9a8d2142671e09ec5d10c1e9ba80467437
renatojs <renato@grupoartway.com.br>
2013-09-11 12:36:55 -0300

    versao 2 - plugin para usar com json

--- src/js/animaWeb.js
+++ src/js/animaWeb.js
@@ -1,73 +1,67 @@
 /*
-/ * animaWeb.js 1.0
+/ * animaWeb.js 2.0
 / *
 / * @author: Renato Santos
 / * www.grupoartway.com.br
 / * github.com/renato-js/animaWeb.git
 / * Date 09/09/2013
-/ *
+/ * Date 11/09/2013
 */
 
 // HOW TO USE
-// anima("caixaContentDoElemento","elementoAnimado","distanciaBottomParaAnimacaoComecar");
+// ...
 
 
-//init
-// window.onscroll = function () {
-// 	animaWeb();
-// };
-
-// //animacao de temporizoacao
-// function anima (_elContent, _elAnimation ,_posXEvent) {
-
-// 	var _eC = document.getElementById(_elContent);
-// 	var _eA = document.getElementById(_elAnimation);
-
-// 	//calculate the bottom offset
-// 	var posX = (document.body.scrollTop) - (_eC.offsetTop) + (window.innerHeight);
-
-// 	//check if condition is true to do the animation
-// 	if(posX >= _posXEvent)
-// 	{
-// 		//animate - opacity
-// 		_eA.style.webkitTransition = "opacity .3s";
-// 		_eA.style.transition = "opacity .3s";
-// 		_eA.style.opacity = "1";
-// 	}
-// }
-
-
-
-anima = function (parameters) {
+;(function (window,document, undefined) {
 	
+	animaWeb = function (parameters) {
+		//exec
+		window.addEventListener("scroll",animaWeb.checaAnima, false);
+		parameters = merge(parameters);
+	}
+
+	//
+	animaWeb.mostraTexto = function (parameters) {
+		//merge
+		parameters = merge(parameters);
+	}
+
+	//
+	animaWeb.checaAnima = function (parameters) {
+		parameters = merge(parameters);
+
+	 	var _eC = document.getElementById(parameters.content);
+	 	var _eA = document.getElementById(parameters.elemento);
+	 	var _posXEvent = parameters.inicio;
+
+		//calculates the bottom offset
+		var posX = (document.body.scrollTop) - (_eC.offsetTop) + (window.innerHeight);
+
+		//check if condition is true to do the animation
+		if(posX >= _posXEvent)
+		{
+			//animate - opacity
+			_eA.style.webkitTransition = "opacity .3s";
+			_eA.style.transition = "opacity .3s";
+			_eA.style.opacity = "1";
+			window.removeEventListener("scroll",animaWeb.checaAnima);	//limpa listener event
+		}
+	}
+
+
+	//propriedades default
+	animaWeb.propDefault = {
+		"texto": "sem texto",
+		"content": "body",
+		"elemento": "body",
+		"inicio": 0
+	}
+
 	//merge
-	//parameters = merge(parameters);
+	function merge (parameters) {
+		return parameters = $.extend(animaWeb.propDefault, parameters);
+	}
 
-	//exec
-	window.onscroll = anima.mostraTexto;
-}
-
-//
-anima.mostraTexto = function (parameters) {
-	//merge
-	parameters = merge(parameters);
-
-	//exec
-	console.log(parameters.texto);
-}
-
-
-
-//propriedades default
-anima.propDefault = {
-	"texto": "sem texto"
-}
-
-
-//
-function merge (parameters) {
-	return parameters = $.extend(anima.propDefault, parameters);
-}
-
+})(window,document);
 
 
