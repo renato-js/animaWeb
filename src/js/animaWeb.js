ebc3c53e77b70b6b4887b1f1028938422cca01ba
renatojs <renato@grupoartway.com.br>
2013-09-11 11:30:42 -0300

    plugin basico

--- src/js/animaWeb.js
+++ src/js/animaWeb.js
@@ -13,26 +13,61 @@
 
 
 //init
-window.onscroll = function () {
-	animaWeb();
-};
+// window.onscroll = function () {
+// 	animaWeb();
+// };
 
-//animacao de temporizoacao
-function anima (_elContent, _elAnimation ,_posXEvent) {
+// //animacao de temporizoacao
+// function anima (_elContent, _elAnimation ,_posXEvent) {
 
-	var _eC = document.getElementById(_elContent);
-	var _eA = document.getElementById(_elAnimation);
+// 	var _eC = document.getElementById(_elContent);
+// 	var _eA = document.getElementById(_elAnimation);
 
-	//calculate the bottom offset
-	var posX = (document.body.scrollTop) - (_eC.offsetTop) + (window.innerHeight);
+// 	//calculate the bottom offset
+// 	var posX = (document.body.scrollTop) - (_eC.offsetTop) + (window.innerHeight);
 
-	//check if condition is true to do the animation
-	if(posX >= _posXEvent)
-	{
-		//animate - opacity
-		_eA.style.webkitTransition = "opacity .3s";
-		_eA.style.transition = "opacity .3s";
-		_eA.style.opacity = "1";
-	}
+// 	//check if condition is true to do the animation
+// 	if(posX >= _posXEvent)
+// 	{
+// 		//animate - opacity
+// 		_eA.style.webkitTransition = "opacity .3s";
+// 		_eA.style.transition = "opacity .3s";
+// 		_eA.style.opacity = "1";
+// 	}
+// }
+
+
+
+anima = function (parameters) {
+	
+	//merge
+	//parameters = merge(parameters);
+
+	//exec
+	window.onscroll = anima.mostraTexto;
 }
+
+//
+anima.mostraTexto = function (parameters) {
+	//merge
+	parameters = merge(parameters);
+
+	//exec
+	console.log(parameters.texto);
+}
+
+
+
+//propriedades default
+anima.propDefault = {
+	"texto": "sem texto"
+}
+
+
+//
+function merge (parameters) {
+	return parameters = $.extend(anima.propDefault, parameters);
+}
+
+
 
