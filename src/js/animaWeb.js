0be7651e6701e2e9c7375aada9db8d014ab7e8bf
unknown <renato.santos@DT01VRT040.taticas.local>
2013-10-31 11:29:04 -0200

    v3.1 - added 'before function'

--- src/js/animaWeb.js
+++ src/js/animaWeb.js
@@ -1,21 +1,19 @@
 /*
-/ * animaWeb.js 3.0
+/ * animaWeb.js 3.1
 / *
 / * @author: Renato Santos
 / * www.grupoartway.com.br
-/ * github.com/renato-js/animaWeb.git
+/ * github.com/renato-js/animaWeb
 / * Date 09/09/2013
 / * Date 11/09/2013
 / * Date 27/09/2013
 / * Date 21/10/2013
-/ / v2.4 - it not more Jquery plugin, add this in all your project without Jquery library!
-// BUG: its only works if you apply to one DIV.
+// v2.4 - it's not more Jquery plugin, add this in all your project without Jquery library!
 // v3.0 - you can apply a function to many ID elements on you page. This events can be call once.
+// v3.1 - add function BEFORE. Now you can call some function before the scrool down
 */ 
 
 // HOW TO USE
-//
-
 
 ;(function (window,document,undefined) {
 	
@@ -28,7 +26,7 @@
 		_$ = function (_elementoID) {return document.getElementById(_elementoID);}
 
 	//constructor
-	animaWeb = function (_elem, _off, _fun) {
+	animaWeb = function (_elem, _off, _fun,_funBefore) {
 
 		if (isIE) {
 			window.attachEvent("onscroll",animaWeb.checaAnimacaoIe);			
@@ -36,6 +34,9 @@
 		else {
 			window.addEventListener("scroll",animaWeb.checaAnimacao, false);
 		}
+
+		//execute function before
+		if(arguments[3] != undefined)_funBefore();
 
 		//update vars
 		sElementos[sElementos.length] = _$(_elem);
